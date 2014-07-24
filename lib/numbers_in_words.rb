eccd79220655c1db2537a23289c913b0377a1360
Austin Steward & Scott Dudley <student@epicodus.com>
2014-07-24 11:49:30 -0700

    Works up to 10 billion digit

--- lib/numbers_in_words.rb
+++ lib/numbers_in_words.rb
@@ -1,5 +1,5 @@
 def numbers_in_words digits
-  digit_hash = {1 => "one", 2 => "two", 3 => "three", 4 => "four", 5 => "five", 6 => "six", 7 => "seven", 8 => "eight",
+  digit_hash = {0 => "", 1 => "one", 2 => "two", 3 => "three", 4 => "four", 5 => "five", 6 => "six", 7 => "seven", 8 => "eight",
             9 => "nine", 10 => "ten", 11 => "eleven", 12 => "twelve", 13 => "thirteen", 14 => "fourteen",
             15 => "fifteen", 16 => "sixteen", 17 => "seventeen", 18 => "eighteen", 19 => "nineteen", 20 => "twenty",
             30 => "thirty", 40 => "forty", 50 => "fifty", 60 => "sixty", 70 => "seventy", 80 => "eighty", 90 => "ninety"}
@@ -8,5 +8,32 @@ def numbers_in_words digits
     solution = digit_hash.fetch(digits)
   elsif digits < 100
     solution = digit_hash.fetch((digit_string[0]+"0").to_i) + " " + digit_hash.fetch(digit_string[1].to_i)
+  elsif digits < 1_000
+    solution = digit_hash.fetch(digit_string[0].to_i) + " hundred " + numbers_in_words(digit_string[1..-1].to_i)
+  elsif digits < 10_000
+    solution = digit_hash.fetch(digit_string[0].to_i) + " thousand " + numbers_in_words(digit_string[1..-1].to_i)
+  elsif digits < 100_000
+    solution = numbers_in_words((digit_string[0..1]).to_i) + " thousand " + numbers_in_words(digit_string[2..-1].to_i)
+  elsif digits < 1_000_000
+    if(digit_string[1..2] == "00")
+      thousand_var = "thousand "
+      else
+      thousand_var = ""
+    end
+    solution = digit_hash.fetch(digit_string[0].to_i) + " hundred " + thousand_var + numbers_in_words(digit_string[1..-1].to_i)
+  elsif digits < 10_000_000
+    solution = digit_hash.fetch(digit_string[0].to_i) + " million " + numbers_in_words(digit_string[1..-1].to_i)
+  elsif digits < 100_000_000
+    solution = numbers_in_words((digit_string[0..1]).to_i) + " million " + numbers_in_words(digit_string[2..-1].to_i)
+  elsif digits < 1_000_000_000
+    if(digit_string[1..2] == "00")
+      million_var = "million "
+      else
+      million_var = ""
+    end
+    solution = digit_hash.fetch(digit_string[0].to_i) + " hundred " + million_var + numbers_in_words(digit_string[1..-1].to_i)
+  elsif digits < 10_000_000_000
+    solution = digit_hash.fetch(digit_string[0].to_i) + " billion " + numbers_in_words(digit_string[1..-1].to_i)
   end
+  solution.strip
 end
--- spec/numbers_in_words_spec.rb
+++ spec/numbers_in_words_spec.rb
@@ -5,7 +5,40 @@ describe("numbers_in_words") do
   it("takes a single digit number and converts it to a written word") do
     numbers_in_words(4).should(eq("four"))
   end
+
   it("takes a two-digit number and converts it to a written word") do
     numbers_in_words(24).should(eq("twenty four"))
+  end
+
+  it("takes a number between 100 and 999 and converts it to a written word") do
+    numbers_in_words(300).should(eq("three hundred"))
+  end
+
+  it("Returns the written form of a number less than 10_000") do
+    numbers_in_words(9_090).should(eq("nine thousand ninety"))
+  end
+
+  it("returns the written form of a number less than 100_000") do
+    numbers_in_words(18_900).should(eq("eighteen thousand nine hundred"))
+  end
+
+  it("returns the written form of a number less than 1_000_000") do
+    numbers_in_words(190_900).should(eq("one hundred ninety thousand nine hundred"))
+  end
+
+  it("returns the written form of a number less than 10_000_000") do
+    numbers_in_words(8_900_700).should(eq("eight million nine hundred thousand seven hundred"))
+  end
+
+  it("returns the written form of a number less than 100_000_000") do
+    numbers_in_words(18_900_700).should(eq("eighteen million nine hundred thousand seven hundred"))
+  end
+
+  it("returns the written form of a number less than 1_000_000_000") do
+    numbers_in_words(180_900_550).should(eq("one hundred eighty million nine hundred thousand five hundred fifty"))
+  end
+
+  it("returns the written form of a number less than 10_000_000_000") do
+    numbers_in_words(9_456_423_123).should(eq("nine billion four hundred fifty six million four hundred twenty three thousand one hundred twenty three"))
   end
 end
